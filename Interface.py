54299e5a4432013888964c08c62033d85e623cef
Cody <143535212+Kodiac314@users.noreply.github.com>
2025-11-23 09:38:08 -0600

    Update Interface.py
    Added picture for  Zips game.

--- Interface.py
+++ Interface.py
@@ -43,8 +43,7 @@ def render() -> None: # basically Macro to plug in arguments
     
     # Sudoku logo
     lf, up = SCREEN_W - tile - buffer, buffer
-    render_logo(lf, up, ORANGE)
-    
+    render_logo(lf, up, BG_BLUE)
     pygame.draw.line(dis, BLACK, (lf + tile*0.2 - THIN/4, up + tile*0.5), (lf + tile*0.8 + THIN/4, up + tile*0.5), width=THIN)
     pygame.draw.line(dis, BLACK, (lf + tile*0.5, up + tile*0.2 - THIN/4), (lf + tile*0.5, up + tile*0.8 + THIN/4), width=THIN)
     for i in range(3):
@@ -52,11 +51,22 @@ def render() -> None: # basically Macro to plug in arguments
     
     # Zips logo
     up += tile + buffer
-    render_logo(lf, up, (12, 77, 207))
+    render_logo(lf, up, ORANGE)
+    for i in range(5):
+        x = [0.35, 0.5, 0.5, 0.5, 0.65][i]
+        y = [0.35, 0.35, 0.5, 0.65, 0.65][i]
+        pygame.draw.circle(dis, ORANGE, (lf+tile*x, up+tile*y), tile*0.1)
+    for i in range(3):
+        x = [0.35, 0.5, 0.5, 0.5, 0.5, 0.65][2*i:2*i+2]
+        y = [0.35, 0.35, 0.35, 0.65, 0.65, 0.65][2*i:2*i+2]
+        pygame.draw.line(dis, ORANGE,
+                        (lf+tile*x[0], up+tile*y[0]),
+                        (lf+tile*x[1], up+tile*y[1]), width=int(tile*0.2))
     
     # 3rd logo
     up += tile + buffer
     render_logo(lf, up, (25, 87, 6))
+    
     
     # 4th logo
     up += tile + buffer
@@ -114,42 +124,4 @@ while run:
             game.mouse_drag(dis, tile, buffer, is_dragged, clicked)
             render()
             
-#             x, y = find(pygame.mouse.get_pos())
-#             
-#             # Clicked on board
-#             if 0 <= x < W and 0 <= y < H:
-#                 # Find board indices
-#                 r, c = int(y / V_SIZE), int(x / SIZE)
-#                 for i in (r-1, r, r+1):
-#                     pc, pr = c * SIZE, i * V_SIZE
-#                     pts = [(pc, pr), (pc + SIZE, pr - V_SIZE), (pc + SIZE, pr + V_SIZE)] if (c+i)%2 else [(pc, pr - V_SIZE), (pc, pr + V_SIZE), (pc + SIZE, pr)]
-#                     if contains((x, y), *pts):
-#                         r = i
-#                         break
-#                 # Update board        
-#                 board.arr[r][c] = color_idx
-#                 render(board, palette, color_idx)
-#                 continue
-#             
-#             # Clicked to change color
-#             SZ, INC = 2/3 * Board.BUFFER * SIZE, 1/6 * Board.BUFFER * SIZE
-#             if W+INC <= x < B-INC and INC <= y < H-INC:
-#                 for i in range(len(palette)):
-#                     lf, up = W + INC, i * (SZ + INC)
-#                     if lf <= x < lf + SZ and up <= y < up + SZ:
-#                         color_idx = i
-#                         break
-#             
-#             # Clicked to restart level
-#             lf, up = W + INC, len(palette) * (SZ + INC)
-#             if lf <= x < lf + SZ and up <= y < up + SZ:
-#                 if board.solved():
-#                     puzzle_idx = min(puzzle_idx + 1, len(PUZZLES)-1)
-#                 board = Board(PUZZLES[puzzle_idx])
-#     
-#             # render(board, Palette.GREYS, color_idx)
-
 pygame.quit()
-
-
-
